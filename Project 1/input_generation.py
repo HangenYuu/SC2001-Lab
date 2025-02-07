cb98c25377eb0c5387824dd6d263c7caf547e568
Pham Nguyen Hung <97870091+HangenYuu@users.noreply.github.com>
2025-02-07 01:22:20 +0000

    Working on project 1

--- Project 1/input_generation.py
+++ Project 1/input_generation.py
@@ -1,12 +1,12 @@
-# Generate a random list of 10 million integers between 0 and 10^6
-# and write them to a file called input.txt
-
 import random
 
-# Generate a list of 10 million random integers between 0 and 10^6
-random_list = [random.randint(0, 10**6) for i in range(10**7)]
+def generate_input(size: int, max_val: int) -> list:
+    return [random.randint(0, max_val) for i in range(size)]
 
-# Write the list to a file called input.txt
-with open('input.txt', 'w') as f:
-    for num in random_list:
-        f.write(str(num) + '\n')
+sizes = [10**3, 10**4, 10**5, 10**6, 10**7]
+
+for size in sizes:
+    with open(f"input_size_{size}.txt", 'w') as file:
+        for item in generate_input(size, size):
+            file.write(f"{item}\n")
+    print(f"Generated input of size {size} and wrote it to input_size_{size}.txt")
